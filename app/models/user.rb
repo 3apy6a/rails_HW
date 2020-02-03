3eabed0c46705df40b31660c5d80b9982578f47e
3apy6a <zaruba.av@gmail.com>
2020-02-03 23:16:40 +0200

    models

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :posts,
+           dependent: :destroy,
+           foreign_key: :author_id
+  has_many :comments,
+           dependent: :destroy,
+           foreign_key: :author_id
+  has_many :income_comments, through: :posts, class_name: 'Comment'
+end
